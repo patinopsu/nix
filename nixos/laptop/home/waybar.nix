0407e34bdeb7caf5882691a9bcdaed0dcd195f64
Patin Muangjan <patin@patin.dev>
2026-01-04 01:18:16 +0700

    give up on waybar

--- nixos/laptop/home/waybar.nix
+++
@@ -1,7 +0,0 @@
-{
-  programs.waybar = {
-    enable =  true;
-    xdg.configFile."../../../waybar/config.jsonc".source = ./waybar/config.jsonc;
-    xdg.configFile."../../../waybar/style.css".source = ./waybar/style.css;
-  };
-}
