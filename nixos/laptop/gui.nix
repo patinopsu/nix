90f341254929c2a3e24375d914ba81ceda6578b7
Patin Muangjan <patin@patin.dev>
2026-01-03 17:45:18 +0700

    remove stylix from gui.nix

--- nixos/laptop/gui.nix
+++ nixos/laptop/gui.nix
@@ -1,42 +1,7 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-  stylix.image = /home/patin/Pictures/walls/anime_skull.png;
-  stylix.polarity = "dark";
-
-  stylix = {
-    fonts = {
-      serif = {
-        name = "Noto Serif";
-      };
-
-      sansSerif = {
-        name = "Noto Sans";
-      };
-
-      monospace = {
-        name = "CaskaydiaCove Nerd Font Mono";
-      };
-
-      emoji = {
-        name = "Noto Color Emoji";
-      };
-      sizes = {
-        desktop = 10;
-        applications = 11;
-      };
-    };
-    icons = {
-      enable = true;
-      package = pkgs.papirus-icon-theme;
-      dark = "Papirus-Dark";
-      light = "Papirus-Light";
-    };
-  };
   programs.zsh.enable = true;
-
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
