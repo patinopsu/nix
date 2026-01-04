bc8ca769e3c014104132135fdf351082394f5721
Patin Muangjan <patin@patin.dev>
2026-01-04 22:22:46 +0700

    FEAT: increase computer security (Networking)

--- nixos/laptop/network.nix
+++ nixos/laptop/network.nix
@@ -1,7 +1,43 @@
 { config, lib, pkgs, ... }:
 
+let
+  nextdnsHost = builtins.readFile /etc/nextdns-id;
+in
+
 {
-  networking.hostName = "atlanta";
-  networking.wireless.enable = true;
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "atlanta";
+    wireless.enable = true;
+    nameservers = [
+      "45.90.28.0#${nextdnsHost}"
+      "45.90.30.0#${nextdnsHost}"
+      "2a07:a8c0::#${nextdnsHost}"
+      "2a07:a8c1::#${nextdnsHost}"
+    ];
+    networkmanager = {
+      enable = true;
+      dhcp = "dhcpcd";
+      dns = "systemd-resolved";
+      wifi = {
+        backend = "wpa_supplicant";
+        macAddress = "stable-ssid";
+      };
+    };
+    firewall = {
+      enable =  true;
+    };
+  };
+  services = {
+    resolved = {
+      enable = true;
+      dnssec = "true";
+      dnsovertls = "true";
+      fallbackDns = [
+        "9.9.9.9"
+        "149.112.112.112"
+        "2620:fe::fe"
+        "2620:fe::9"
+      ];
+    };
+  };
 }
