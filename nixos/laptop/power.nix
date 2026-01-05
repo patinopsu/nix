1b2544148561839b9fe7f3e9a56f5482159497de
Patin Muangjan <patin@patin.dev>
2026-01-05 09:50:10 +0700

    CHANGE: power management

--- nixos/laptop/power.nix
+++ nixos/laptop/power.nix
@@ -6,10 +6,16 @@
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "balance_performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
 
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+
+        CPU_BOOST_ON_AC = "1";
+        CPU_BOOST_ON_BAT = "1";
+
+        CPU_HWP_DYN_BOOST_ON_AC = "1";
+        CPU_HWP_DYN_BOOST_ON_BAT = "1";
 
         RESTORE_DEVICE_STATE_ON_STARTUP = "1";
 
@@ -22,6 +28,12 @@
 
         "START_CHARGE_THRESH_BAT0" = "80";
         "STOP_CHARGE_THRESH_BAT0" = "97";
+
+        "PLATFORM_PROFILE_ON_AC" = "performance";
+        "PLATFORM_PROFILE_ON_BAT" = "balanced";
+
+        "MEM_SLEEP_ON_AC" = "s2idle";
+        "MEM_SLEEP_ON_BAT" = "deep";
       };
     };
   };
