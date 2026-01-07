34415a9efde884f3216275075789c7a6290bc5a6
Patin Muangjan <patin@patin.dev>
2026-01-07 20:52:04 +0700

    FEAT: use intel GVT-g

--- nixos/laptop/virtualization.nix
+++ nixos/laptop/virtualization.nix
@@ -1,6 +1,18 @@
 {
   virtualisation = {
+    kvmgt = {
+      enable = true;
+      vgpus = {
+        "i915-GVTg_V5_8" = {
+          uuid = [ "a45bcaa2-eb87-11f0-8b39-6f2b6719fb9c" ];
+        };
+      };
+    };
+    libvirtd.enable = true;
     virtualbox.host.enable = true;
     docker.enable = true;
+  };
+  programs = {
+    virt-manager.enable = true;
   };
 }
