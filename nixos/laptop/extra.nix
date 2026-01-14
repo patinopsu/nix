d6a3a5e662853f9bd773738ddd9048fa73d15ae7
Patin Muangjan <patin@patin.dev>
2026-01-14 12:22:40 +0700

    1Password: remove custom_allowed_browsers

--- nixos/laptop/extra.nix
+++ nixos/laptop/extra.nix
@@ -5,13 +5,4 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
   time.timeZone = "Asia/Bangkok";
-
-  environment.etc = {
-    "1password/custom_allowed_browsers" = {
-      text = ''
-        .librewolf-wrap
-        librewolf
-      '';
-    };
-  };
 }
