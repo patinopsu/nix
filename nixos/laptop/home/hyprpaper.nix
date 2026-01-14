4d172c28a11c214072cdd0457771358aa3c6fb54
Patin Muangjan <patin@patin.dev>
2026-01-14 21:20:45 +0700

    Stylix: move to dedicated hyprpaper config

---
+++ nixos/laptop/home/hyprpaper.nix
@@ -0,0 +1,12 @@
+{
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      wallpaper = {
+          monitor = eDP-1;
+          path = ~/walls/tt2.jpg;
+          fit_mode = cover;
+      };
+    }
+  };
+}
