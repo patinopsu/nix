1e89ef14bc3176c8ccfdc1e595d9742e8dbfcaab
Patin Muangjan <patin@patin.dev>
2026-01-15 22:36:28 +0700

    Hyprpaper:

--- nixos/laptop/home/hyprpaper.nix
+++ nixos/laptop/home/hyprpaper.nix
@@ -8,11 +8,13 @@
     wallpaper {
         monitor = eDP-1
         path = ~/walls/tt2.jpg
+        #path = ~/Pictures/lpb.jpg
         fit_mode = cover
     }
 
     wallpaper {
-        monitor = 
+        monitor =
+        #path = ~/walls/tt2.jpg
         path = ~/walls/archivesgov-911-2.JPG
         fit_mode = cover
     }
