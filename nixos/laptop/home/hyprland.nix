fc38905926482a20d624c875b240b2fc4703aa9a
Patin Muangjan <patin@patin.dev>
2026-01-21 17:35:12 +0700

    T14G1-Hyprland: remove env

--- nixos/laptop/home/hyprland.nix
+++ nixos/laptop/home/hyprland.nix
@@ -22,11 +22,6 @@
         "noctalia-shell"
       ];
 
-      env = [
-        "XCURSOR_SIZE,24"
-        "HYPRCURSOR_SIZE,24"
-      ];
-
       general = {
         gaps_in = 5;
         gaps_out = 20;
