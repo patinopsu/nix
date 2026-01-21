5774f8b1705bcf52f72b9464935fa19a97c16d1b
Patin Muangjan <patin@patin.dev>
2026-01-21 22:03:07 +0700

    T14Gen1-displayManager: merge config files with gui.nix

--- laptop/gui.nix
+++ laptop/gui.nix
@@ -1,6 +1,20 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "colormix";
+      battery_id = "BAT0";
+      bigclock = "en";
+      box_title = "Patin's ThinkPad T14 Gen 1";
+      brightness_down_key = "";
+      brightness_up_key = "";
+      clock = "%c";
+      edge_margin = "1";
+    };
+  };
+  systemd.services.display-manager.environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
   programs = {
     zsh.enable = true;
     hyprland = {
