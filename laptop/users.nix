27c759766fb17e5f856a7dd30bbcedf496da99ff
Patin Muangjan <patin@patin.dev>
2026-01-22 13:13:20 +0700

    T14Gen1-User: should attach this is the commit head fc90a94d

--- laptop/users.nix
+++ laptop/users.nix
@@ -24,6 +24,7 @@
         ./home/hyprshell.nix
         ./home/hypridle.nix
         ./home/hyprlock.nix
+        ./home/fastfetch.nix
         ./home/extra.nix
         inputs.arkenfox.hmModules.arkenfox
         inputs.noctalia.homeModules.default
