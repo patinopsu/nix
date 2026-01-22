be3936fb91af077c661e8662458c4d8fd6237a2f
Patin Muangjan <patin@patin.dev>
2026-01-22 23:08:45 +0700

    T14Gen1-Pipewire: fix audio stutter when system is under extreme load (hopefully)

--- laptop/audio.nix
+++ laptop/audio.nix
@@ -13,10 +13,12 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
-      extraConfig.pipewire."92-low-latency" = {
+      extraConfig.pipewire."99-stability" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
+          "default.clock.quantum" = 1024;
           "default.clock.min-quantum" = 1024;
+          "default.clock.max-quantum" = 2048;
         };
       };
       
